19cac036f1904eda97c2b90f7952efa6688ed3d8
Barnaby Malet <bmalet@gmail.com>
2012-02-17 13:24:42 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carbonmonitor::Application.config.secret_token = 'da87fd718212d065b25e30cb2d73bb91935a54d1c5a30505ee01a1ac11097aec01d7cab89079d5d81eb93ef482abe4a7d89a4e5c99a07d56b13045d399c26ac8'
